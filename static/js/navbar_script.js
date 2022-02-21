bee6bbc12c236018a8bc415c1f2b5247df9c0cd2
revandrazm <mahartarevandra@gmail.com>
2022-02-21 21:35:34 +0700

    added: comments

--- static/js/navbar_script.js
+++ static/js/navbar_script.js
@@ -1,7 +1,9 @@
+// open new tab on click
 function openNewTab(link) {
     window.open(link, "_blank")
 }
 
+// show time in id: time
 var update = function () {
     document.querySelector("#time").innerHTML = moment().format("h:mm:ss A");
  };
