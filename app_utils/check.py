9aaca1e3178a67fb3f424af2f9e04fe40ea624b1
revandrazm <mahartarevandra@gmail.com>
2022-03-06 18:32:37 +0700

    added: username_exist_check function

--- app_utils/check.py
+++ app_utils/check.py
@@ -1,6 +1,7 @@
 import sqlite3
 import bcrypt
 
+
 def account_exist_check(username: str, password: str):
     """Check if an account exist; return True if exist"""
     
@@ -11,3 +12,11 @@ def account_exist_check(username: str, password: str):
             return bcrypt.checkpw(password, cursor.fetchone()[0])
     except TypeError:
         return False
+    
+def username_exist_check(username: str):
+    """Check if an username exist; return True if exist"""
+    
+    with sqlite3.connect("data.db") as conn:
+        cursor = conn.cursor()
+        cursor.execute("""SELECT EXISTS(SELECT 1 FROM accounts WHERE username = ?)""", (username,))
+        return cursor.fetchone()[0] == 1
