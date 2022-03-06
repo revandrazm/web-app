539301efeefe601563043724782e777eaeb26e29
revandrazm <mahartarevandra@gmail.com>
2022-03-06 18:25:56 +0700

    added: select_table_where function

--- app_utils/table.py
+++ app_utils/table.py
@@ -4,6 +4,8 @@ import sqlite3
 
 
 def create_table():
+    """Create table if it doesn't exist"""
+    
     if not os.path.exists("data.db"):
         with sqlite3.connect("data.db") as conn:
             cursor = conn.cursor()
@@ -18,3 +20,13 @@ def select_table(fileName: str):
         cursor.execute("SELECT * FROM accounts")
         result = [dict(row) for row in cursor.fetchall()]
         return result
+    
+def select_table_where(fileName: str, username: str):
+    """Select all row from table accounts with where"""
+    
+    with sqlite3.connect(fileName) as conn:
+        conn.row_factory = sqlite3.Row
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM accounts WHERE username = ?", (username,))
+        result = [dict(row) for row in cursor.fetchall()]
+        return result[0]
