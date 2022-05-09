67407ed872914d8c9611f39392fc37786b4ef4b5
revandrazm <mahartarevandra@gmail.com>
2022-05-09 16:50:12 +0700

    improved

--- static/js/form_script.js
+++ static/js/form_script.js
@@ -1,6 +1,6 @@
 // clear class: error-message on keydown on any input
 document.querySelectorAll("input").forEach(item => {
-	item.addEventListener("keydown", function() {
+	item.addEventListener("keydown", () => {
 		document.querySelector(".error-message").innerHTML = "<br>";
 	});
 });
